1e49c0fcbfbb16c1e5986f44ca0c2f3f8a54a087
kirillHvesyukovich <krillx94@gmail.com>
2025-08-10 18:22:16 +0200

    second JS homework

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,25 @@
   преобразовываясь в number
 
 */
+const minAge = 18;
+const maxAge = 60;
+let age = "30"
+
+ if (isNaN(age)){
+    console.log ("Incorrect data type");
+  }
+else{
+  age = +age;
+if (age < minAge){
+  console.log(`You don\'t have access cause your age is ${age} it\'s less then ${minAge}`)
+}
+else if ( age >= minAge && age< maxAge){
+  console.log("Welcome !");
+}
+else if (age> maxAge){
+  console.log("Keep calm and look Culture channel")
+}
+else{
+  console.log("Technical work");
+}
+}
